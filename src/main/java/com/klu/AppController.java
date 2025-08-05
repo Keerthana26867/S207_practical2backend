da8aa0de1289f9033d32c5f921757340e1b11f28
Keerthana Reddy <2300033874@kluniversity.in>
2025-08-05 12:16:59 +0530

    Update AppController.java

--- src/main/java/com/klu/AppController.java
+++ src/main/java/com/klu/AppController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 @CrossOrigin
 public class AppController {
